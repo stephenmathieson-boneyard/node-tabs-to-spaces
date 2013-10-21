990807a96425310355d3da43e57db95d78316d2a
Stephen Mathieson <smath23@gmail.com>
2013-10-21 10:42:49 -0400

    Initial commit

---
+++ bin.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+var convert = require('./');
+var program = require('commander');
+
+program
+  .version(require('./package.json').version)
+  .option('-s, --spaces [number]', 'Number of spaces to use, defaults to 2', 2)
+  .option('-v, --verbose', 'Verbose output', false)
+  .parse(process.argv);
+
+var files = program.args;
+var verbose = program.verbose;
+var spaces = program.spaces;
+
+if (!files.length) {
+  process.exit(0);
+}
+
+next(0);
+
+function next(index) {
+  var file = files[index];
+  if (!file) return process.exit(0);
+  if (verbose) console.log('converting %s', file);
+  convert.file(file, spaces, function (err) {
+    if (err) throw err;
+    index++;
+    next(index);
+  });
+}
